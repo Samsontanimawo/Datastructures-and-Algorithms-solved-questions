bf6da0fe839bdeb55ddd33fc619164b4ae8e7a8f
Samson Tanimawo <samsontanimawo4jbs@yahoo.com>
2023-08-05 19:35:41 -0500

    Time: 22 ms (84.78%) | Memory: 13.4 MB (30.89%) - LeetSync

---
+++ 83-remove-duplicates-from-sorted-list/remove-duplicates-from-sorted-list.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def deleteDuplicates(self, head):
+        
+        cur = head
+        
+        while cur and cur.next:
+            if cur.val == cur.next.val:
+                cur.next = cur.next.next
+                
+            else:
+                cur = cur.next
+                
+        return head
