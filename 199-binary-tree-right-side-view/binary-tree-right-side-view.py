7ea778bc5abb25ba420b42d7cfabb1ee2569eb4c
Samson Tanimawo <samsontanimawo4jbs@yahoo.com>
2023-08-05 19:37:51 -0500

    Time: 15 ms (83.42%) | Memory: 13.3 MB (46.23%) - LeetSync

---
+++ 199-binary-tree-right-side-view/binary-tree-right-side-view.py
@@ -0,0 +1,20 @@
+class Solution:
+    def rightSideView(self, root):
+        if not root:
+            return []
+        
+        queue, result, level = [root], [], []
+        
+        while queue:
+            for node in queue:
+                if node.left:
+                    level.append(node.left)
+                    
+                if node.right:
+                    level.append(node.right)
+                    
+            result.append(node.val)
+            queue = level
+            level = []
+            
+        return result
