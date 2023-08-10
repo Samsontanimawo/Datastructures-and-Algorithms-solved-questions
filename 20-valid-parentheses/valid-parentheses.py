b0aebf5f98b18e9af6408ad388b53d67c9ea21e2
Samson Tanimawo <samsontanimawo4jbs@yahoo.com>
2023-08-10 18:35:35 -0500

    Time: 21 ms (32.89%) | Memory: 13.5 MB (47.41%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.py
+++ 20-valid-parentheses/valid-parentheses.py
@@ -2,25 +2,17 @@ class Solution(object):
     def isValid(self, s):
         
         stack = []
-        mapping = { ')':'(', '}':'{', ']':'[' }
-        
+        mapBrackets = { ')':'(', '}':'{', ']':'[' }
+
         for char in s:
-            if char in mapping:
-                if stack and stack[-1] == mapping[char]:
+            if char in mapBrackets:
+                if stack and stack[-1] == mapBrackets[char]:
                     stack.pop()
                     
                 else:
                     return False
-                
-                
+
             else:
                 stack.append(char)
-                
-                
+
         return stack == []
-    
-    """
-    O(N) Time and Space
-    
-    """
-            
