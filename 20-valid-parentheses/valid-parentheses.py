c23a413a50fa7c99b7fd19bc869d48b057bfbde7
Samson Tanimawo <samsontanimawo4jbs@yahoo.com>
2023-08-10 18:28:51 -0500

    Time: 12 ms (88.83%) | Memory: 13.3 MB (96.53%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.py
+++ 20-valid-parentheses/valid-parentheses.py
@@ -1,19 +1,26 @@
-class Solution:
+class Solution(object):
     def isValid(self, s):
-        # Dictionary to store the mappings of open and close brackets
-        brackets = {'(':')', '[':']', '{':'}'}
+        
         stack = []
-
-        # Iterate through the string
+        mapping = { ')':'(', '}':'{', ']':'[' }
+        
         for char in s:
-            # If the character is an open bracket, push it to the stack
-            if char in brackets:
-                stack.append(char)
-            # If the character is a close bracket
-            elif char in brackets.values():
-            # If the stack is empty or the top of the stack is not a corresponding open bracket, return False
-                if not stack or brackets[stack.pop()] != char:
+            if char in mapping:
+                if stack and stack[-1] == mapping[char]:
+                    stack.pop()
+                    
+                else:
                     return False
-
-        # Return True if the stack is empty, False otherwise
-        return not stack
+                
+                
+            else:
+                stack.append(char)
+                
+                
+        return stack == []
+    
+    """
+    O(N) Time and Space
+    
+    """
+            
