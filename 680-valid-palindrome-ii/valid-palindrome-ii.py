56f53c362d176514f48a7661e83e418064eb893e
Samson Tanimawo <samsontanimawo4jbs@yahoo.com>
2023-11-26 05:09:23 -0600

    Time: 104 ms (45.59%) | Memory: 14.1 MB (40.12%) - LeetSync

--- 680-valid-palindrome-ii/valid-palindrome-ii.py
+++ 680-valid-palindrome-ii/valid-palindrome-ii.py
@@ -1,7 +1,6 @@
 class Solution(object):
     def validPalindrome(self, s):
         def palindromeCheck(left, right):
-            
             while left < right:
                 if s[left] != s[right]:
                     return False
@@ -9,10 +8,11 @@ class Solution(object):
                 else:
                     left +=1
                     right -=1
-
             return True
 
-        left, right = 0, len(s)-1
+        left = 0
+        right = len(s)-1
+
         while left < right:
             if s[left] != s[right]:
                 return palindromeCheck(left + 1, right) or palindromeCheck(left, right - 1)
@@ -20,7 +20,4 @@ class Solution(object):
             else:
                 left +=1
                 right -=1
-
         return True
-
-    
