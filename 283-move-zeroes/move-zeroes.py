489c5c2801c59641bc4bb2260cdcdcad20f4ad0d
Samson Tanimawo <samsontanimawo4jbs@yahoo.com>
2023-12-03 17:37:14 -0600

    Time: 117 ms (58.99%) | Memory: 14.6 MB (17.88%) - LeetSync

--- 283-move-zeroes/move-zeroes.py
+++ 283-move-zeroes/move-zeroes.py
@@ -1,12 +1,13 @@
 class Solution:
     def moveZeroes(self, nums):
         
-        
+        # nums = [0,1,0,3,12]
+        # Output: [1,3,12,0,0]
+
         left = 0
         
+
         for right in range(len(nums)):
             if nums[right] != 0:
-                nums[right], nums[left] = nums[left], nums[right]
-                left +=1             
-                
-                
+                nums[left], nums[right] = nums[right], nums[left]
+                left +=1
