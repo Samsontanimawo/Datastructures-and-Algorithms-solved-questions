b133b94448b937a64a81b017246cecb736527e94
Samson Tanimawo <samsontanimawo4jbs@yahoo.com>
2023-12-07 04:30:32 -0600

    Time: 76 ms (34.04%) | Memory: 21.9 MB (58.16%) - LeetSync

--- 1790-lowest-common-ancestor-of-a-binary-tree-iii/lowest-common-ancestor-of-a-binary-tree-iii.py
+++ 1790-lowest-common-ancestor-of-a-binary-tree-iii/lowest-common-ancestor-of-a-binary-tree-iii.py
@@ -1,11 +1,13 @@
 class Solution(object):
     def lowestCommonAncestor(self, p, q):
+        pCopy = p
+        qCopy = q
 
-      pCopy = p
-      qCopy = q
+        while pCopy != qCopy:
+          pCopy = pCopy.parent if pCopy else q
+          qCopy = qCopy.parent if qCopy else p
 
-      while pCopy != qCopy:
-        pCopy = pCopy.parent if pCopy else q
-        qCopy = qCopy.parent if qCopy else p
+        return pCopy or qCopy
+
+        # O(N) TIME. O(1) Space
         
-      return pCopy or qCopy
