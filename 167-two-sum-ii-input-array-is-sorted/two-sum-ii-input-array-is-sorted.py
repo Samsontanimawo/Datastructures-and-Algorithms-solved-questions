cc92b6ad96a3fcdb0955daf53c876bb8f52848aa
Samson Tanimawo <samsontanimawo4jbs@yahoo.com>
2023-12-09 15:25:37 -0600

    Time: 92 ms (43.90%) | Memory: 14.3 MB (14.07%) - LeetSync

--- 167-two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.py
+++ 167-two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.py
@@ -1,13 +1,12 @@
 class Solution(object):
     def twoSum(self, numbers, target):
-        left = 0
-        right = len(numbers)-1
+        left, right = 0, len(numbers)-1
 
         while left < right:
             currentSum = numbers[left] + numbers[right]
-            
+
             if currentSum == target:
-                return [left + 1, right + 1]
+                return [left + 1, right+1]
 
             elif currentSum < target:
                 left +=1
@@ -15,4 +14,3 @@ class Solution(object):
             else:
                 right -=1
 
-        return []
