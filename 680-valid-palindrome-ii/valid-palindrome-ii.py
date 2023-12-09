6fa51f3cc075625607b1f93ae5b72d261f33f1af
Samson Tanimawo <samsontanimawo4jbs@yahoo.com>
2023-12-09 17:54:25 -0600

    Time: 106 ms (42.80%) | Memory: 14 MB (68.50%) - LeetSync

--- 680-valid-palindrome-ii/valid-palindrome-ii.py
+++ 680-valid-palindrome-ii/valid-palindrome-ii.py
@@ -1,36 +1,27 @@
 class Solution(object):
     def validPalindrome(self, s):
-
-        # Helper function to check if a substring is a palindrome
         def palindromeCheck(left, right):
+
             while left < right:
-                # If characters don't match, it's not a palindrome
                 if s[left] != s[right]:
                     return False
-                else:
-                    # Move pointers towards the center
-                    left += 1
-                    right -= 1
 
-            # If the loop completes, the substring is a palindrome
+                else:
+                    left +=1
+                    right -=1
+
             return True
 
-        # Initialize pointers at the beginning and end of the string
-        left, right = 0, len(s) - 1
+        left, right = 0, len(s)-1
 
-        # Iterate through the string from both ends towards the center
         while left < right:
-            
-            # If characters don't match
             if s[left] != s[right]:
-                # Check if making either of the characters equal will result in a palindrome
-                return palindromeCheck(left + 1, right) or palindromeCheck(left, right - 1)
-            else:
-                # Move pointers towards the center
-                left += 1
-                right -= 1
+                return palindromeCheck(left +1, right) or palindromeCheck(left, right-1)
 
-        # If the loop completes, the string is a valid palindrome
+            else:
+                left +=1
+                right -=1
+
         return True
 
-# print(Solution().validPalindrome("ABAHDDDDADFA"))
+        # O(N) time | O(1) Space
