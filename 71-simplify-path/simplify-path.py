337b931bd85506a8aeef863691e20752b26ffd87
Samson Tanimawo <samsontanimawo4jbs@yahoo.com>
2023-12-09 16:13:26 -0600

    Time: 16 ms (69.28%) | Memory: 13.4 MB (40.06%) - LeetSync

--- 71-simplify-path/simplify-path.py
+++ 71-simplify-path/simplify-path.py
@@ -1,13 +1,16 @@
 class Solution:
     def simplifyPath(self, path):
         
+        # Split the path using '/'
         paths = path.split("/")
         stack = []
         
+        # Ignore empty and '.' components
         for char in paths:
             if char == "." or not char:
                 continue
-                
+
+        # Handle '..' by popping the last directory from the stack (if not empty) 
             elif char == "..":
                 if stack:
                     stack.pop()
