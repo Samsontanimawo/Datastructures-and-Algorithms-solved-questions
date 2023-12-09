8a37da04a4f33b2006baad887a3d87cf7d02bfe8
Samson Tanimawo <samsontanimawo4jbs@yahoo.com>
2023-12-09 14:31:20 -0600

    Time: 99 ms (56.57%) | Memory: 20.9 MB (76.84%) - LeetSync

--- 347-top-k-frequent-elements/top-k-frequent-elements.py
+++ 347-top-k-frequent-elements/top-k-frequent-elements.py
@@ -1,35 +1,48 @@
+from typing import List
+from collections import Counter
+
 class Solution:
-    def topKFrequent(self, nums, k):
-        
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        # Step 1: Count the frequency of each element in the input list
         freq = Counter(nums)
-        unique = list(freq.keys())
-        k = len(unique)-k
         
+        # Step 2: Create a list of unique elements
+        unique = list(freq.keys())
+        
+        # Calculate the position where the kth frequent element would be in the sorted list
+        k = len(unique) - k
+        
+        # Step 3: Define a helper function for quick selection
         def quickSelect(left, right):
+            # Choose the pivot element (rightmost element in this case)
             pivot = unique[right]
             partition = left
             
+            # Partition the unique list based on the frequency of elements
             for index in range(left, right):
                 if freq[unique[index]] <= freq[pivot]:
                     unique[index], unique[partition] = unique[partition], unique[index]
-                    partition +=1
+                    partition += 1
                     
+            # Move the pivot element to its final sorted position
             unique[partition], unique[right] = unique[right], unique[partition]
             
+            # Recursively perform quick select on the appropriate partition
             if partition > k:
                 return quickSelect(left, partition - 1)
-            
             elif partition < k:
                 return quickSelect(partition + 1, right)
-            
             else:
                 return unique[partition]
             
-        quickSelect(0, len(unique)-1)
+        # Step 4: Perform quick selection to find the kth frequent element
+        quickSelect(0, len(unique) - 1)
+        
+        # Step 5: Return the top k frequent elements
         return unique[k:]
-    
-    
-    """
-    O(N) TIME
-    O(1) SPACE
-    """
+
+# Additional information in comments
+"""
+O(N) TIME
+O(1) SPACE
+"""
