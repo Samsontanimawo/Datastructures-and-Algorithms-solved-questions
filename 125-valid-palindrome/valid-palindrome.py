95f0f240840730be623c7f4316dd1ae47b590a08
Samson Tanimawo <samsontanimawo4jbs@yahoo.com>
2023-12-09 17:48:32 -0600

    Time: 16 ms (98.16%) | Memory: 14 MB (86.02%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.py
+++ 125-valid-palindrome/valid-palindrome.py
@@ -1,20 +1,26 @@
 class Solution(object):
     def isPalindrome(self, s):
-        left, right = 0, len(s)-1
+        # Initialize two pointers, 'left' at the beginning and 'right' at the end of the string
+        left, right = 0, len(s) - 1
 
+        # Continue the loop as long as 'left' pointer is less than 'right' pointer
         while left < right:
+            # Move 'left' pointer to the right until a valid alphanumeric character is found
             while left < right and not s[left].isalnum():
-                left +=1
+                left += 1
 
+            # Move 'right' pointer to the left until a valid alphanumeric character is found
             while left < right and not s[right].isalnum():
-                right -=1
+                right -= 1
 
+            # Check if the characters at 'left' and 'right' positions are not equal (case-insensitive)
             if s[left].lower() != s[right].lower():
-                return False
+                return False  # If not equal, it's not a palindrome
 
             else:
-                left +=1
-                right -=1
+                # Move both pointers inward as the characters are equal
+                left += 1
+                right -= 1
 
+        # If the loop completes without returning False, the string is a palindrome
         return True
-        
