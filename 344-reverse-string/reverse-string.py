fad7f2b38f134ec3f3178107636b134bde748bb3
Samson Tanimawo <samsontanimawo4jbs@yahoo.com>
2023-12-09 05:54:12 -0600

    Time: 138 ms (25.96%) | Memory: 21 MB (27.14%) - LeetSync

--- 344-reverse-string/reverse-string.py
+++ 344-reverse-string/reverse-string.py
@@ -1,9 +1,18 @@
 class Solution(object):
     def reverseString(self, s):
-        left, right = 0, len(s)-1
-        while left < right:
-            s[left], s[right] = s[right], s[left]
-            left +=1
-            right -=1
+               # Initialize two pointers, left and right, pointing to the start and end of the string
+        left = 0
+        right = len(s) - 1
 
+        # Continue swapping characters from the outer ends towards the center
+        while left < right:
+            # Swap the characters at the left and right pointers
+            s[left], s[right] = s[right], s[left]
+
+            # Move the pointers towards the center
+            left += 1
+            right -= 1
+
+        # Return the length of the reversed string (not the reversed string itself)
         return len(s)
+        
