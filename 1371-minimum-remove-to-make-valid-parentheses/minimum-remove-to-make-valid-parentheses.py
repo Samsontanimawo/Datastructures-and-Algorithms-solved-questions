86b0b8bbca8c7ee51af078f230d8eefd80a1382b
Samson Tanimawo <samsontanimawo4jbs@yahoo.com>
2023-12-09 06:42:13 -0600

    Time: 61 ms (93.31%) | Memory: 18.9 MB (21.87%) - LeetSync

--- 1371-minimum-remove-to-make-valid-parentheses/minimum-remove-to-make-valid-parentheses.py
+++ 1371-minimum-remove-to-make-valid-parentheses/minimum-remove-to-make-valid-parentheses.py
@@ -1,22 +1,19 @@
-# ()))
-# "lee(t(c)o)de)"
-# [   (     ]
-
 class Solution(object):
     def minRemoveToMakeValid(self, s):
-        stack, s, N = [], list(s), len(s)
+        s = list(s)
+        stack = []
 
-        for index in range(N):
-          if s[index] == "(":
-            stack.append(index)
+        for index in range(len(s)):
+            if s[index] == "(":
+                stack.append(index)
 
-          elif s[index] == ")":
-            if stack:
-              stack.pop()
-            else:
-              s[index] = ""
-              
-              
+            elif s[index] == ")":
+                if stack:
+                    stack.pop()
+
+                else:
+                    s[index] = ""
+
         for index in stack:
             s[index] = ""
 
