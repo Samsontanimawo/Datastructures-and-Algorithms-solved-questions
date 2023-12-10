dfc6f84d1a962d3d4662e8aca6a20bf4879d4dd7
Samson Tanimawo <samsontanimawo4jbs@yahoo.com>
2023-12-10 09:04:55 -0600

    Time: 495 ms (96.80%) | Memory: 29.1 MB (47.00%) - LeetSync

--- 1909-buildings-with-an-ocean-view/buildings-with-an-ocean-view.py
+++ 1909-buildings-with-an-ocean-view/buildings-with-an-ocean-view.py
@@ -1,27 +1,18 @@
 class Solution(object):
     def findBuildings(self, heights):
-        # Initialize variables to keep track of the maximum height and the result list
-        maxHeight, result = 0, []
-
-        # Check if the input list is empty
         if not heights:
             return None
 
-        # Iterate through the heights in reverse order (from right to left)
-        for index in range(len(heights) - 1, -1, -1):
+        result = []
+        maxHeight = 0
 
-            # Check if the current building is taller than the previous maximum height
-            if heights[index] > maxHeight:
-
-                # If yes, add the index to the result list and update the maximum height
+        for index in range(len(heights)-1, -1, -1):
+            if maxHeight < heights[index]:
                 result.append(index)
                 maxHeight = heights[index]
 
-        # Reverse the result list to obtain the order from left to right
-        return result[::-1] # Result = [ 4 3 1 ] = [1 3 4] = Return the index = [0, 2, 3]
+        return result [::-1]
 
-# Example usage:
-#solution_instance = Solution()
-#result = solution_instance.findBuildings([4, 2, 3, 1])
-#print(result)
+        # O(N) Time and space
 
+        
