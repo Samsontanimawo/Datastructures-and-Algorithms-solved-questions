75d2ee83d26e05083186d3214f44c04c89ad4ef4
Samson Tanimawo <samsontanimawo4jbs@yahoo.com>
2023-12-10 17:50:08 -0600

    Time: 46 ms (58.41%) | Memory: 15 MB (11.66%) - LeetSync

--- 1-two-sum/two-sum.py
+++ 1-two-sum/two-sum.py
@@ -1,25 +1,29 @@
-class Solution:
+class Solution(object):
     def twoSum(self, nums, target):
-    # Create a dictionary to store the indices of elements
-        num_indices = {}
-
-        for i, num in enumerate(nums):
-        # Calculate the complement needed to reach the target
-            complement = target - num
-
-            # Check if the complement is in the dictionary
-            if complement in num_indices:
-            # Return the indices of the two numbers
-                return [num_indices[complement], i]
-
-        # Store the current number and its index in the dictionary
-            num_indices[num] = i
-
-    # If no solution is found
-        return None
+        # Enumerate the sorted array to keep track of the original indices.
+        sorted_nums = sorted(enumerate(nums), key=lambda x: x[1])
+    
+        # Initialize two pointers.
+        left, right = 0, len(nums) - 1
+    
+        while left < right:
+            current_sum = sorted_nums[left][1] + sorted_nums[right][1]
+        
+            # Check if the sum is equal to the target.
+            if current_sum == target:
+                # Return the original indices of the two numbers.
+                return [sorted_nums[left][0], sorted_nums[right][0]]
+        
+        # If the sum is less than the target, move the left pointer to the right.
+            elif current_sum < target:
+                left += 1
+        
+        # If the sum is greater than the target, move the right pointer to the left.
+            else:
+                right -= 1
 
 # Example usage:
-#nums = [2, 7, 11, 15]
-#target = 9
-#result = twoSum(nums, target)
-#print(result)  # Should print [0, 1]
+# nums = [2, 7, 11, 15]
+# target = 9
+# result = twoSum(nums, target)
+# print(result)
