c9bfa70e6657ffac86f54972c418f3d0b8aec55c
Samson Tanimawo <samsontanimawo4jbs@yahoo.com>
2023-12-10 07:24:11 -0600

    Time: 76 ms (72.63%) | Memory: 19.1 MB (10.03%) - LeetSync

--- 1371-minimum-remove-to-make-valid-parentheses/minimum-remove-to-make-valid-parentheses.py
+++ 1371-minimum-remove-to-make-valid-parentheses/minimum-remove-to-make-valid-parentheses.py
@@ -1,29 +1,24 @@
+#Input: s = "))((" | (()))
+# Stack = [           ]
+#Output: ""
+
 class Solution(object):
     def minRemoveToMakeValid(self, s):
-        # Convert the input string to a list for easier modification
-        convertStringToList = list(s)
-        # Initialize a stack to keep track of indices of open parentheses
+        s = list(s)
         stack = []
 
-        # Iterate through the characters of the string
         for index in range(len(s)):
-            # If an open parenthesis is encountered, push its index onto the stack
-            if convertStringToList[index] == "(":
+            if s[index] == "(":
                 stack.append(index)
 
-            # If a closing parenthesis is encountered
-            elif convertStringToList[index] == ")":
-                # Check if there is a matching open parenthesis in the stack
+            elif s[index] == ")":
                 if stack:
-                    stack.pop()  # Remove the matching open parenthesis index from the stack
+                    stack.pop()
+
                 else:
-                    # If no matching open parenthesis is found, replace the current closing parenthesis with an empty string
-                    convertStringToList[index] = ""
+                    s[index] = ""
 
-        # Iterate through the remaining open parenthesis indices in the stack
         for index in stack:
-            # Replace the open parenthesis at the index with an empty string
-            convertStringToList[index] = ""
+            s[index] = ""
 
-        # Join the modified list to obtain the final result
-        return "".join(convertStringToList)
+        return "".join(s)
