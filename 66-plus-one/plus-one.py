9345bc0457e4207af5cc93ae4d850ec899dc2621
Samson Tanimawo <samsontanimawo4jbs@yahoo.com>
2023-12-09 19:55:17 -0600

    Time: 14 ms (73.26%) | Memory: 13.3 MB (49.64%) - LeetSync

---
+++ 66-plus-one/plus-one.py
@@ -0,0 +1,20 @@
+class Solution(object):
+    def plusOne(self, digits):
+        length = len(digits) - 1
+        
+        while digits[length] == 9:            
+            digits[length] = 0
+            length -= 1
+            
+        if length < 0:
+            digits = [1] + digits
+            
+        else:
+            digits[length] += 1
+            
+        return digits
+    
+    """
+    O(N) Time
+    O(1) Space
+    """
