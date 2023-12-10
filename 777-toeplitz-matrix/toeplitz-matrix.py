83eee5bff2a8da3d09e67de700df266ccf842851
Samson Tanimawo <samsontanimawo4jbs@yahoo.com>
2023-12-09 18:02:35 -0600

    Time: 49 ms (95.76%) | Memory: 13.5 MB (11.44%) - LeetSync

--- 777-toeplitz-matrix/toeplitz-matrix.py
+++ 777-toeplitz-matrix/toeplitz-matrix.py
@@ -1,13 +1,11 @@
-class Solution:
+class Solution(object):
     def isToeplitzMatrix(self, matrix):
-        # Iterate through each row (except the last one)
-        for row in range(len(matrix) - 1):
-            # Iterate through each column (except the last one)
-            for col in range(len(matrix[0]) - 1):
-                # Check if the current element is not equal to the diagonal element
-                if matrix[row][col] != matrix[row + 1][col + 1]:
-                    # If not equal, it's not a Toeplitz matrix
+        for row in range(len(matrix)-1):
+            for col in range(len(matrix[0])-1):
+                if matrix[row][col] != matrix[row+1][col + 1]:
                     return False
 
-        # If all elements passed the check, it's a Toeplitz matrix
         return True
+
+                
+        
