4ee8b53024763793eb9ab94d8025f1a194a8ba88
Samson Tanimawo <samsontanimawo4jbs@yahoo.com>
2023-12-09 18:42:26 -0600

    Time: 90 ms (96.36%) | Memory: 18.2 MB (50.72%) - LeetSync

--- 56-merge-intervals/merge-intervals.py
+++ 56-merge-intervals/merge-intervals.py
@@ -1,15 +1,27 @@
 class Solution:
     def merge(self, intervals):
+        # Define indices for start and end in the interval tuples
+        start, end, result = 0, 1, []
         
-        start, end, result = 1, -1, []
-        
+        # Sort the intervals based on their start times
         intervals.sort()
         
+        # Iterate through the sorted intervals
         for interval in intervals:
-            if result == [] or result[end][start] < interval[0]:
+            # If the result is empty or the current interval does not overlap with the last merged interval
+            if result == [] or result[-1][end] < interval[start]:
+                # Add the non-overlapping interval to the result
                 result.append(interval)
-                
             else:
-                result[end][start] = max(result[end][start], interval[start])
+                # Merge the overlapping intervals by updating the end of the last merged interval
+                result[-1][end] = max(result[-1][end], interval[end])
                 
         return result
+
+# Example usage:
+#solution = Solution()
+#intervals1 = [[1,3],[2,6],[8,10],[15,18]]
+#print(solution.merge(intervals1))  # Output: [[1,6],[8,10],[15,18]]
+
+#intervals2 = [[1,4],[4,5]]
+#print(solution.merge(intervals2))  # Output: [[1,5]]
