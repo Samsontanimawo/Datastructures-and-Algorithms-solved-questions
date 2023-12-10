e9c304a88b108901a053a60050de8951fb42dcb2
Samson Tanimawo <samsontanimawo4jbs@yahoo.com>
2023-12-10 03:22:49 -0600

    Time: 23 ms (23.17%) | Memory: 13.5 MB (40.06%) - LeetSync

--- 71-simplify-path/simplify-path.py
+++ 71-simplify-path/simplify-path.py
@@ -1,15 +1,12 @@
-class Solution:
+class Solution(object):
     def simplifyPath(self, path):
-        # Split the path using '/'
         paths = path.split("/")
         stack = []
 
-        # Ignore empty and '.' components
         for char in paths:
             if char == "." or not char:
                 continue
 
-            # Handle '..' by popping the last directory from the stack (if not empty)
             elif char == "..":
                 if stack:
                     stack.pop()
@@ -17,5 +14,5 @@ class Solution:
             else:
                 stack.append(char)
 
-        # Construct the simplified canonical path by joining the directories with '/'
         return "/" + "/".join(stack)
+        
