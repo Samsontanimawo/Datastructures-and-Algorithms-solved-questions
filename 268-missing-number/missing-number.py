e7349f2b4203f73224e6094f3fda3aaa67c371aa
Samson Tanimawo <samsontanimawo4jbs@yahoo.com>
2023-12-10 17:40:27 -0600

    Time: 94 ms (69.55%) | Memory: 14.6 MB (32.22%) - LeetSync

--- 268-missing-number/missing-number.py
+++ 268-missing-number/missing-number.py
@@ -1,14 +1,9 @@
 class Solution:
     def missingNumber(self, nums):
-        # Initialize missingNumber with the length of the array,
-        # assuming that the missing number is initially at the end of the range.
-        missingNumber = len(nums)
-        
-        # Iterate through the array using both index and value.
-        for index, num in enumerate(nums):
-            # Update missingNumber by subtracting the current element value and
-            # adding the current index, essentially comparing it with the expected sum.
-            missingNumber += index - num
-            
-        # After the loop, missingNumber will hold the actual missing number.
-        return missingNumber
+        n = len(nums)
+
+        expected_sum = n * (n + 1) // 2
+        actual_sum = sum(nums)
+
+        return expected_sum - actual_sum
+
