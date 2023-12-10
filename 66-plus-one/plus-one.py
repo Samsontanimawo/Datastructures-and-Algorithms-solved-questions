b22ed8d71bc233ffaa95b3385c7ffbdf6f5ff1c7
Samson Tanimawo <samsontanimawo4jbs@yahoo.com>
2023-12-09 19:56:00 -0600

    Time: 16 ms (50.73%) | Memory: 13.3 MB (20.85%) - LeetSync

--- 66-plus-one/plus-one.py
+++ 66-plus-one/plus-one.py
@@ -1,20 +1,14 @@
 class Solution(object):
     def plusOne(self, digits):
-        length = len(digits) - 1
-        
-        while digits[length] == 9:            
-            digits[length] = 0
-            length -= 1
-            
-        if length < 0:
-            digits = [1] + digits
-            
-        else:
-            digits[length] += 1
-            
+        n = len(digits)
+        carry = 1  # Initialize carry to 1 to represent the increment
+
+        for i in range(n - 1, -1, -1):
+            total = digits[i] + carry
+            digits[i] = total % 10  # Update the current digit
+            carry = total // 10  # Update the carry
+
+        if carry:
+            digits.insert(0, carry)  # If there is a carry left, insert a new digit at the beginning
+
         return digits
-    
-    """
-    O(N) Time
-    O(1) Space
-    """
