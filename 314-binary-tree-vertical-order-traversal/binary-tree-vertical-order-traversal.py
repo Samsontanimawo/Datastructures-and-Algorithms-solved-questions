33fb09e8405fc0e899661af3ed6011a0977040b6
Samson Tanimawo <samsontanimawo4jbs@yahoo.com>
2023-12-10 16:40:09 -0600

    Time: 20 ms (36.24%) | Memory: 13.7 MB (7.21%) - LeetSync

--- 314-binary-tree-vertical-order-traversal/binary-tree-vertical-order-traversal.py
+++ 314-binary-tree-vertical-order-traversal/binary-tree-vertical-order-traversal.py
@@ -1,10 +1,11 @@
 class Solution(object):
     def verticalOrder(self, root):
+        minColumn = maxColumn = 0
+        hashmap = defaultdict(list)
+        queue = deque([(root, 0)])
 
         if not root:
-            return []
-
-        minColumn, maxColumn, queue, hashmap = 0, 0, deque([(root, 0)]), defaultdict(list)
+            return None
 
         while queue:
             node, columnIndex = queue.popleft()
@@ -16,8 +17,12 @@ class Solution(object):
 
             if node.left:
                 queue.append((node.left, columnIndex - 1))
-
+            
             if node.right:
-                queue.append((node.right, columnIndex + 1))
+                queue.append((node.right, columnIndex +1 ))
 
         return [hashmap[index] for index in range(minColumn, maxColumn + 1)]
+
+        # O(N) Time and space
+
+        
