18b5311d4b1d013e8d36d2bcdd4d93dbdc127646
Samson Tanimawo <samsontanimawo4jbs@yahoo.com>
2023-12-10 20:43:17 -0600

    Time: 12 ms (90.50%) | Memory: 13.4 MB (18.47%) - LeetSync

--- 65-valid-number/valid-number.py
+++ 65-valid-number/valid-number.py
@@ -1,43 +1,37 @@
-class Solution:
+class Solution(object):
     def isNumber(self, s):
-        # Initialize flags for different components of a number
-        digit = decimal = symbol = exponent = False
-        
-        # Iterate through each character in the input string
+        decimal = exponent = digit = symbol = False
+        # decimal = .
+        # exponent = eE
+        # digit = 5667
+        # symbol = +-
+
         for char in s:
-            # Check if the character is a digit
             if char.isdigit():
                 digit = True
-                
-            # Check if the character is a symbol (+ or -)
-            elif char in '+-':
-                # Symbols are only allowed at the beginning or after an 'e'/'E'
-                if digit or decimal or symbol:
+
+            elif char in "+-":
+                if digit or decimal or symbol: # If we have them after the symbol
                     return False
                 else:
                     symbol = True
-                
-            # Check if the character is an 'e' or 'E' for exponent notation
-            elif char in 'Ee':
-                # 'e'/'E' should come after at least one digit and only once
+
+            elif char in 'eE':
                 if not digit or exponent:
                     return False
+
                 else:
-                    # Reset flags for digit, decimal, symbol, and set exponent flag
                     digit = decimal = symbol = False
                     exponent = True
-                
-            # Check if the character is a dot (decimal point)
-            elif char == '.':
-                # Dot is only allowed once and before an exponent
-                if decimal or exponent:
-                    return False
+
+            elif char == '.': # decimal
+                if decimal or exponent: # If decimal or exponent comes after decimal
+                     return False
+
                 else:
                     decimal = True
-            
-            # If the character is none of the valid characters, it's not a valid number
+
             else:
                 return False
-            
-        # Check if at least one digit is present
+
         return digit
