e6edd0aa192120668275812edc50e25b75bf2518
Samson Tanimawo <samsontanimawo4jbs@yahoo.com>
2023-12-10 20:29:35 -0600

    Time: 18 ms (26.61%) | Memory: 13.2 MB (49.55%) - LeetSync

--- 50-powx-n/powx-n.py
+++ 50-powx-n/powx-n.py
@@ -1,11 +1,5 @@
-# Anything raised to power 0 = 1
-# Anything raised to -1 = 1/n
-# Negative 
-# Positive
-
 class Solution(object):
     def myPow(self, x, n):
-        
         if n == 0:
             return 1
 
@@ -14,12 +8,10 @@ class Solution(object):
 
         if n == -1:
             return 1/x
-       
-       # If n is even, pow(x, n) = pow(x, n/2) * pow(x, n/2).
-        if n%2==0: # 2^4 = 2^2 X 2^2 = 
-            return self.myPow(x, n//2) ** 2
-            
-# If n is odd, pow(x, n) = pow(x, n/2) * pow(x, n/2) * x.
-        else: # Odd numbers = 2^5 = 2^4 X 2^1 = 2^2 X 2^2 X 2^1
+
+        if n%2==0:
+            return self.myPow(x, n//2)**2
+
+        else:
             return self.myPow(x, n//2) * self.myPow(x, n//2 + 1)
         
