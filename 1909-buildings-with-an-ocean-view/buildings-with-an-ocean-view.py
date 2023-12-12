0401d2c49ace3b9aa93b3ef65d46f44e7e4447a2
Samson Tanimawo <samsontanimawo4jbs@yahoo.com>
2023-12-12 07:07:17 -0600

    Time: 518 ms (67.65%) | Memory: 29.2 MB (30.98%) - LeetSync

--- 1909-buildings-with-an-ocean-view/buildings-with-an-ocean-view.py
+++ 1909-buildings-with-an-ocean-view/buildings-with-an-ocean-view.py
@@ -1,18 +1,24 @@
+
+# Input: heights = [4,   2,    3,   1]
+# Index             0    1     2    3
+# Output: [0,2,3]
+
+# maxHeight = 3
+# Result = [  4 3 1 ]
+
+
+
 class Solution(object):
     def findBuildings(self, heights):
         if not heights:
-            return None
+            return []
 
         result = []
         maxHeight = 0
 
-        for index in range(len(heights)-1, -1, -1):
-            if maxHeight < heights[index]:
+        for index in range(len(heights)-1,-1,-1):
+            if heights[index] > maxHeight:
                 result.append(index)
                 maxHeight = heights[index]
 
-        return result [::-1]
-
-        # O(N) Time and space
-
-        
+        return result[::-1]
