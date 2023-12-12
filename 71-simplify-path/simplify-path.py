17209cd771d3b9ecc2d11bf41cd50649afb8a692
Samson Tanimawo <samsontanimawo4jbs@yahoo.com>
2023-12-12 16:59:42 -0600

    Time: 16 ms (69.34%) | Memory: 13.4 MB (70.68%) - LeetSync

--- 71-simplify-path/simplify-path.py
+++ 71-simplify-path/simplify-path.py
@@ -1,22 +1,19 @@
 class Solution(object):
     def simplifyPath(self, path):
-        if not path:
-            return []
-
         stack = []
-
-
         paths = path.split("/")
 
         for path in paths:
             if path == "." or not path:
                 continue
-
+            
             elif path == "..":
                 if stack:
                     stack.pop()
-
+                    
             else:
                 stack.append(path)
 
         return "/" + "/".join(stack)
+        
+        # O(N) TIME AND SPACE
