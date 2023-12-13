108cd82cf06fb61a937bc82d97ea1b4508ce79f0
Samson Tanimawo <samsontanimawo4jbs@yahoo.com>
2023-12-12 18:22:42 -0600

    Time: 109 ms (20.76%) | Memory: 14.8 MB (54.43%) - LeetSync

--- 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.py
+++ 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.py
@@ -10,4 +10,15 @@ class Solution(object):
                 left +=1
               
 
-        return len(nums)
+        return len(nums) # The output will be the length of the numbers. E.g 5
+
+# Example usage
+#nums = [1, 1, 2, 2, 4, 4, 5]
+#result = Solution().removeDuplicates(nums)
+#print(result)
+
+# nums = [1, 1, 2, 2, 4, 4, 5]
+ #            L
+# Indices 0  1  2  3  4  5  6
+
+# [   1 2 4 5 ]
