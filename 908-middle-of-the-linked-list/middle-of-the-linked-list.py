2b3e50051639abd84b32e8389e65532a1f159b14
Samson Tanimawo <samsontanimawo4jbs@yahoo.com>
2023-12-13 10:13:30 -0600

    Time: 20 ms (14.78%) | Memory: 13.2 MB (64.48%) - LeetSync

--- 908-middle-of-the-linked-list/middle-of-the-linked-list.py
+++ 908-middle-of-the-linked-list/middle-of-the-linked-list.py
@@ -1,9 +1,10 @@
 class Solution(object):
     def middleNode(self, head):
-        fast = slow = head # Both should start at the head
+        slow = fast = head
 
-        while fast and fast.next: # Make sure fast and fast.next exist
-            fast = fast.next.next # Fast moves 2 steps
-            slow = slow.next # Slow moves 1 step
+        while fast and fast.next:
+            fast = fast.next.next
+            slow = slow.next
 
-        return slow # Return slow = Middle number or right number
+        return slow
+        
