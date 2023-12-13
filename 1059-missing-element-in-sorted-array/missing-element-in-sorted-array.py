1510c87bd9ed5cb54256a0f922149d4291d42ad9
Samson Tanimawo <samsontanimawo4jbs@yahoo.com>
2023-12-12 18:51:27 -0600

    Time: 204 ms (93.10%) | Memory: 18 MB (68.28%) - LeetSync

--- 1059-missing-element-in-sorted-array/missing-element-in-sorted-array.py
+++ 1059-missing-element-in-sorted-array/missing-element-in-sorted-array.py
@@ -1,15 +1,20 @@
 class Solution(object):
     def missingElement(self, nums, k):
+        # Initialize left and right pointers for binary search
         left = 0
-        right = len(nums)-1
+        right = len(nums) - 1
 
+        # Perform binary search until left and right pointers meet
         while left < right:
+            # Calculate the middle index
             mid = (left + right + 1) // 2
 
+            # Check if the number of missing elements up to mid is less than k
             if nums[mid] - nums[0] - mid < k:
-                left = mid
+                left = mid  # If true, update the left pointer
 
             else:
-                right = mid - 1
+                right = mid - 1  # If false, update the right pointer
 
+        # The result is the sum of the first element, k, and the left pointer
         return nums[0] + k + left
