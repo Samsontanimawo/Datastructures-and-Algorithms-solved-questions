4bb82ad9c8d0ac8f44f32fd0a2a8be557c86756d
Samson Tanimawo <samsontanimawo4jbs@yahoo.com>
2023-12-12 18:44:30 -0600

    Time: 114 ms (18.23%) | Memory: 14.5 MB (73.74%) - LeetSync

--- 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.py
+++ 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.py
@@ -5,20 +5,11 @@ class Solution(object):
         while left < len(nums)-1:
             if nums[left] == nums[left + 1]:
                 del nums[left]
-
+            
             else:
                 left +=1
-              
 
-        return len(nums) # The output will be the length of the numbers. E.g 5
+        return len(nums)
 
-# Example usage
-#nums = [1, 1, 2, 2, 4, 4, 5]
-#result = Solution().removeDuplicates(nums)
-#print(result)
-
-# nums = [1, 1, 2, 2, 4, 4, 5]
- #            L
-# Indices 0  1  2  3  4  5  6
-
-# [   1 2 4 5 ]
+# nums = [0, 0, 1, 1, 1, 2, 2, 3, 3, 4]
+#            L
