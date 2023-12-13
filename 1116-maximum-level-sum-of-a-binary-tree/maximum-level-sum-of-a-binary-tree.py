947821a7d3303f4e4be6b4bff16297b8f9b45407
Samson Tanimawo <samsontanimawo4jbs@yahoo.com>
2023-12-13 06:00:53 -0600

    Time: 239 ms (47.72%) | Memory: 21.5 MB (49.59%) - LeetSync

--- 1116-maximum-level-sum-of-a-binary-tree/maximum-level-sum-of-a-binary-tree.py
+++ 1116-maximum-level-sum-of-a-binary-tree/maximum-level-sum-of-a-binary-tree.py
@@ -8,30 +8,33 @@ class Solution(object):
 
         max_level = 1  # Initialize the level with the maximum sum to 1
         max_sum = float('-inf')  # Initialize the maximum sum to negative infinity
+        level = 1
+        queue = [root]
 
-        queue = deque([(root, 1)])  # Use a deque to perform level order traversal
-
-        # Perform level order traversal using a queue
         while queue:
-            level_size = len(queue)
-            current_sum = 0
+            level_sum = 0
+            next_level = []
 
-            # Process nodes at the current level
-            for _ in range(level_size):
-                node, level = queue.popleft()
-                current_sum += node.val
+            for node in queue:
+                level_sum += node.val
 
-                # Add left and right child nodes to the queue with updated levels
                 if node.left:
-                    queue.append((node.left, level + 1))
+                    next_level.append(node.left)
 
                 if node.right:
-                    queue.append((node.right, level + 1))
+                    next_level.append(node.right)
 
-            # Update the maximum sum and corresponding level
-            if current_sum > max_sum:
-                max_sum = current_sum
+            if level_sum > max_sum:
+                max_sum = level_sum
                 max_level = level
 
-        # Return the level with the maximum sum
+            queue = next_level
+            level +=1
+
         return max_level
+
+
+
+
+
+        
