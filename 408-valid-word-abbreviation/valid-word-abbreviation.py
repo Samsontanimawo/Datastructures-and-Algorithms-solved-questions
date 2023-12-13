bb67eac941c95f71c6e72e0ed78148060979b193
Samson Tanimawo <samsontanimawo4jbs@yahoo.com>
2023-12-13 11:44:55 -0600

    Time: 11 ms (88.72%) | Memory: 13.4 MB (30.08%) - LeetSync

--- 408-valid-word-abbreviation/valid-word-abbreviation.py
+++ 408-valid-word-abbreviation/valid-word-abbreviation.py
@@ -1,21 +1,21 @@
+# word = "internationalization", abbr = "i12iz4n"
+#         L                              R
+
 class Solution(object):
     def validWordAbbreviation(self, word, abbr):
-        # word = "internationalization", abbr = "i12iz4n"
-        #         LEFT                    RIGHT
-        
         left = right = 0
 
         while left < len(word) and right < len(abbr):
             if word[left] != abbr[right]:
                 if not abbr[right].isdigit():
                     return False
-
+                    
                 else:
                     if abbr[right] == "0":
                         return False
-
+                        
                     steps = 0
-
+                    
                     while right < len(abbr) and abbr[right].isdigit():
                         steps = steps * 10 + int(abbr[right])
                         right +=1
@@ -27,3 +27,6 @@ class Solution(object):
                 right +=1
 
         return left == len(word) and right == len(abbr)
+
+            
+        
