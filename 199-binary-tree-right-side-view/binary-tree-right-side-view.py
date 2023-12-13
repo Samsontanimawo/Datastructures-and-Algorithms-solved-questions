bca99ba01d1aa39abc1da9bd0da55a67193ea2c6
Samson Tanimawo <samsontanimawo4jbs@yahoo.com>
2023-12-13 10:52:56 -0600

    Time: 11 ms (90.62%) | Memory: 13.2 MB (63.29%) - LeetSync

--- 199-binary-tree-right-side-view/binary-tree-right-side-view.py
+++ 199-binary-tree-right-side-view/binary-tree-right-side-view.py
@@ -1,20 +1,25 @@
-class Solution:
+class Solution(object):
     def rightSideView(self, root):
         if not root:
             return []
         
-        queue, result, level = [root], [], []
-        
+        rightSideView = []
+        queue = [root]
+
         while queue:
+            nextLevel = []  # Initialize nextLevel for the current level
+
             for node in queue:
                 if node.left:
-                    level.append(node.left)
-                    
+                    nextLevel.append(node.left)
+
                 if node.right:
-                    level.append(node.right)
-                    
-            result.append(node.val)
-            queue = level
-            level = []
-            
-        return result
+                    nextLevel.append(node.right)
+
+            # Append the value of the rightmost node at the current level
+            rightSideView.append(queue[-1].val)
+
+            # Update the queue for the next level
+            queue = nextLevel
+
+        return rightSideView
