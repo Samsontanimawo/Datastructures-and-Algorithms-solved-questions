2dbed4397612e6985baaf400ba431a5a4e7688ca
Samson Tanimawo <samsontanimawo4jbs@yahoo.com>
2023-12-13 11:50:56 -0600

    Time: 20 ms (14.85%) | Memory: 13.4 MB (60.90%) - LeetSync

--- 408-valid-word-abbreviation/valid-word-abbreviation.py
+++ 408-valid-word-abbreviation/valid-word-abbreviation.py
@@ -1,32 +1,22 @@
-# word = "internationalization", abbr = "i12iz4n"
-#         L                              R
-
 class Solution(object):
     def validWordAbbreviation(self, word, abbr):
         left = right = 0
 
         while left < len(word) and right < len(abbr):
             if word[left] != abbr[right]:
-                if not abbr[right].isdigit():
+                if not abbr[right].isdigit() or abbr[right] == "0":
                     return False
-                    
-                else:
-                    if abbr[right] == "0":
-                        return False
-                        
-                    steps = 0
-                    
-                    while right < len(abbr) and abbr[right].isdigit():
-                        steps = steps * 10 + int(abbr[right])
-                        right +=1
 
-                    left += steps
+                steps = 0
+                
+                while right < len(abbr) and abbr[right].isdigit():
+                    steps = steps * 10 + int(abbr[right])
+                    right += 1
+
+                left += steps
 
             else:
-                left +=1
-                right +=1
+                left += 1
+                right += 1
 
         return left == len(word) and right == len(abbr)
-
-            
-        
