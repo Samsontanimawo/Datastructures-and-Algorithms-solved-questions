acd6f58e5102c5a60f5c205a7df4809012e55471
Samson Tanimawo <samsontanimawo4jbs@yahoo.com>
2023-12-13 19:38:56 -0600

    Time: 117 ms (18.91%) | Memory: 15.2 MB (10.89%) - LeetSync

--- 1128-remove-all-adjacent-duplicates-in-string/remove-all-adjacent-duplicates-in-string.py
+++ 1128-remove-all-adjacent-duplicates-in-string/remove-all-adjacent-duplicates-in-string.py
@@ -4,10 +4,11 @@ class Solution(object):
         s = list(s)
 
         for char in s:
-            if stack and stack[-1] == char:
+            if char in stack and stack[-1] == char:
                 stack.pop()
 
             else:
                 stack.append(char)
 
         return "".join(stack)
+# O(N) Time and Space        
