890c925e6d6e48eb2c359904812fa4c8b2d02ca5
Samson Tanimawo <samsontanimawo4jbs@yahoo.com>
2023-12-13 18:58:18 -0600

    Time: 18 ms (26.95%) | Memory: 13.3 MB (22.83%) - LeetSync

--- 50-powx-n/powx-n.py
+++ 50-powx-n/powx-n.py
@@ -3,19 +3,14 @@ class Solution(object):
         if n == 0:
             return 1
 
-        elif n == 1:
+        if n == 1:
             return x
 
-        elif n == -1:
+        if n == -1:
             return 1/x
 
-        elif n%2==0:
-            return self.myPow(x, n//2)**2 
-            # 2^4 = 16
-            # (2 ^ 2) ^ 2 = 16
+        if n%2==0:
+            return self.myPow(x, n//2) **2
 
         else:
             return self.myPow(x, n//2) * self.myPow(x, n//2 + 1)
-
-            
-        
