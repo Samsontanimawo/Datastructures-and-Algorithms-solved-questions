cefc319b5d0f43290a722d0711ea4202517264ed
Samson Tanimawo <samsontanimawo4jbs@yahoo.com>
2023-12-14 08:09:14 -0600

    Time: 492 ms (5.09%) | Memory: 22.8 MB (97.94%) - LeetSync

--- 217-contains-duplicate/contains-duplicate.py
+++ 217-contains-duplicate/contains-duplicate.py
@@ -1,18 +1,13 @@
 class Solution(object):
     def containsDuplicate(self, nums):
-        # Create an empty dictionary to store the frequency of each number.
-        hashmap = {}    # { 1  2 3 4 5 }
+        nums.sort()
+        left = 0
 
-        # Iterate through the list of numbers.
-        for num in nums:
-            # Check if the number is already in the dictionary.
-            # E.g [ 1,1,2,4,5,5 ] = new dict = [         ]
-            if num in hashmap:
-                # If the number is already in the dictionary, it means it's a duplicate.
+        while left < len(nums)-1:
+            if nums[left] == nums[left + 1]:
                 return True
-            else:
-                # If the number is not in the dictionary, add it with a frequency of 1.
-                hashmap[num] = 1
 
-        # If the loop completes without finding any duplicates, return False.
+            else:
+                left +=1
+
         return False
