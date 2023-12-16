f40769c231a22c1bba43ce35800a2d026569d064
Samson Tanimawo <samsontanimawo4jbs@yahoo.com>
2023-12-16 08:41:19 -0600

    Time: 156 ms (80.73%) | Memory: 29.6 MB (23.07%) - LeetSync

---
+++ 975-range-sum-of-bst/range-sum-of-bst.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def rangeSumBST(self, root, low, high):
+
+        if not root:
+            return 0
+
+        if root.val < low:
+            return self.rangeSumBST(root.right, low, high)
+
+        elif root.val > high:
+            return self.rangeSumBST(root.left, low, high)
+
+        else:
+            return root.val + self.rangeSumBST(root.right, low, high) + self.rangeSumBST(root.left, low, high)
