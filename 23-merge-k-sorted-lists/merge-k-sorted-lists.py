0888b3b3ca3e52843118eb94c201293bf7b1f377
Samson Tanimawo <samsontanimawo4jbs@yahoo.com>
2023-12-16 11:01:53 -0600

    Time: 97 ms (28.28%) | Memory: 25.5 MB (5.09%) - LeetSync

---
+++ 23-merge-k-sorted-lists/merge-k-sorted-lists.py
@@ -0,0 +1,34 @@
+class Solution:
+    def mergeKLists(self, lists):
+        
+        length = len(lists)
+        
+        if length == 0:
+            return None
+        
+        if length == 1:
+            return lists[0]
+        
+        mid = length // 2
+        
+        left = self.mergeKLists(lists[:mid])
+        right = self.mergeKLists(lists[mid:])
+        
+        return self.mergeTwoLists(left, right)   
+        
+        
+        
+    def mergeTwoLists(self, list1, list2):
+        if not list1:
+            return list2
+
+        elif not list2:
+            return list1
+
+        elif list1.val < list2.val:
+            list1.next = self.mergeTwoLists(list1.next, list2)
+            return list1
+
+        else:
+          list2.next = self.mergeTwoLists(list2.next, list1)
+          return list2
