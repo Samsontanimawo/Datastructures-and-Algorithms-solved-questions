6e3b554d8eaeaa07de12bd75a3fb642ba8dab77c
Samson Tanimawo <samsontanimawo4jbs@yahoo.com>
2023-12-15 18:29:53 -0600

    Time: 104 ms (75.66%) | Memory: 14.3 MB (81.91%) - LeetSync

--- 724-find-pivot-index/find-pivot-index.py
+++ 724-find-pivot-index/find-pivot-index.py
@@ -1,10 +1,10 @@
 class Solution(object):
     def pivotIndex(self, nums):
-        total_sum = sum(nums)
+        total_sum = sum(nums) # Sum of all the numbers = 1,7,3,6,5,6 = 28
         left_sum = 0
 
         for index in range(len(nums)):
-            total_sum -= nums[index]
+            total_sum = total_sum - nums[index]
 
             if left_sum == total_sum:
                 return index
