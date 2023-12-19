9db934de1779a27309969b82a3c081d5bfe0a866
Samson Tanimawo <samsontanimawo4jbs@yahoo.com>
2023-12-18 19:05:24 -0600

    Time: 104 ms (29.35%) | Memory: 25.9 MB (5.27%) - LeetSync

--- 23-merge-k-sorted-lists/merge-k-sorted-lists.py
+++ 23-merge-k-sorted-lists/merge-k-sorted-lists.py
@@ -2,19 +2,19 @@ class Solution(object):
     def mergeKLists(self, lists):
         length = len(lists)
 
-        if not lists or length == 0:
+        if not length or len(lists) == 0:
             return None
 
         if length == 1:
             return lists[0]
 
-        mid = length//2
+        mid = length // 2
 
         left = self.mergeKLists(lists[:mid])
         right = self.mergeKLists(lists[mid:])
 
         return self.mergeTwoLists(left, right)
-
+    
     def mergeTwoLists(self, list1, list2):
         if not list1:
             return list2
@@ -25,7 +25,7 @@ class Solution(object):
         elif list1.val < list2.val:
             list1.next = self.mergeTwoLists(list1.next, list2)
             return list1
-
+            
         else:
             list2.next = self.mergeTwoLists(list2.next, list1)
             return list2
