e3de098d4c909aa5df541552552a01b518ab2635
Samson Tanimawo <samsontanimawo4jbs@yahoo.com>
2023-12-20 10:27:47 -0600

    Time: 74 ms (74.87%) | Memory: 19 MB (23.21%) - LeetSync

--- 1371-minimum-remove-to-make-valid-parentheses/minimum-remove-to-make-valid-parentheses.py
+++ 1371-minimum-remove-to-make-valid-parentheses/minimum-remove-to-make-valid-parentheses.py
@@ -1,22 +1,32 @@
+#Input: s = "lee(t(c)o)de)"
+# Input: s = "l   e   e   (   t  (  c  )  o  ) d   e   )"
+#             0   1   2   3  4   5  6  7  8  9 10  11  12
+# STACK = [l e   e   t c o  d   e )]
+# New stack = l   e   e   (   t  (  c  )  o  ) d   e
+# Convert the list to s = lee(t(c)o)de
+# Output = "lee(t(c)o)de"
+
 class Solution(object):
     def minRemoveToMakeValid(self, s):
-        s = list(s)
-        stack = []
+       s = list(s)
+       n = len(s)
+       stack = []
 
-        for index in range(len(s)):
-            if s[index] == "(":
-                stack.append(index)
+       for index in range(n):
+           if s[index] == "(":
+               stack.append(index)
 
-            elif s[index] == ")":
-                if stack:
-                    stack.pop()
+           elif s[index] == ")":
+               if stack:
+                   stack.pop()
 
-                else:
+               else:
                     s[index] = ""
 
-        for index in stack:
+           #else:
+               # stack.append(index)
+
+       for index in stack:
             s[index] = ""
 
-        return "".join(s)
-
-    #   O(N) Time and SPACE
+       return "".join(s)
