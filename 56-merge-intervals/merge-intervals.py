9a33d3a853a6e144a8fd1657c00e2e10f8de0266
Samson Tanimawo <samsontanimawo4jbs@yahoo.com>
2023-12-21 06:06:13 -0600

    Time: 118 ms (11.26%) | Memory: 18.3 MB (43.10%) - LeetSync

--- 56-merge-intervals/merge-intervals.py
+++ 56-merge-intervals/merge-intervals.py
@@ -1,19 +1,20 @@
 class Solution(object):
     def merge(self, intervals):
-
-        intervals.sort()
-
         if not intervals:
             return []
 
-        result = []
         start, end = 1, -1
+        result = []
+
+        intervals.sort()
 
         for interval in intervals:
-            if not result or result[end][start] < interval[0]:
+            if result == [] or result[end][start] < interval[0]:
                 result.append(interval)
 
             else:
                 result[end][start] = max(result[end][start], interval[end])
 
         return result
+
+# O(nlogN) time | O(1) Space
