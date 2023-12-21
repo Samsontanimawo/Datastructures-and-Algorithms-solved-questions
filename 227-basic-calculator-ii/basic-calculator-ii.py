25151dcc193c670f209f22208b294771365268b1
Samson Tanimawo <samsontanimawo4jbs@yahoo.com>
2023-12-21 06:41:56 -0600

    Time: 119 ms (89.04%) | Memory: 86.5 MB (5.28%) - LeetSync

--- 227-basic-calculator-ii/basic-calculator-ii.py
+++ 227-basic-calculator-ii/basic-calculator-ii.py
@@ -1,29 +1,3 @@
 class Solution(object):
     def calculate(self, s):
-        stack = []
-        num = 0
-        operation = "+"
-        
-        for char in s + "+":
-            if char.isdigit():
-                num = num * 10 + int(char)
-                
-            elif char in '+-*/':
-                if operation == "+":
-                    stack.append(num)
-
-                elif operation == "-":
-                    stack.append(-num)
-
-                elif operation == "*":
-                    stack[-1] *= num
-
-                elif operation == "/":
-
-                    # In Python 3, use integer division //
-                    stack[-1] = int(stack[-1] / float(num))
-                    
-                operation = char
-                num = 0
-                
-        return sum(stack)
+        return eval(s)
