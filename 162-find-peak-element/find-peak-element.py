42fb4cedd553902e1e127ebeb77fd7368083a4ff
Samson Tanimawo <samsontanimawo4jbs@yahoo.com>
2023-12-21 20:57:38 -0600

    Time: 28 ms (45.90%) | Memory: 13.5 MB (28.88%) - LeetSync

--- 162-find-peak-element/find-peak-element.py
+++ 162-find-peak-element/find-peak-element.py
@@ -6,11 +6,9 @@ class Solution(object):
             mid = (left + right)//2
 
             if nums[mid] < nums[mid + 1]:
-                left = mid  +1 
+                left = mid + 1
 
             else:
-                right = mid 
+                right = mid
 
         return left or right
-
-# O(logN) time. O(1) SPACE
