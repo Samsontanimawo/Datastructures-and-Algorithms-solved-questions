ecf8f78420173e5a8b33705f2bf4f5b1b3bb1071
Samson Tanimawo <samsontanimawo4jbs@yahoo.com>
2024-01-01 15:57:29 -0800

    Time: 130 ms (5.13%) | Memory: 13.3 MB (71.40%) - LeetSync

--- 777-toeplitz-matrix/toeplitz-matrix.py
+++ 777-toeplitz-matrix/toeplitz-matrix.py
@@ -1,11 +1,9 @@
 class Solution(object):
     def isToeplitzMatrix(self, matrix):
-       if not matrix:
-           return []
-
-       for row in range(len(matrix)-1):
-           for col in range(len(matrix[0])-1):
-               if matrix[row][col] != matrix[row + 1][col+1]:
+        for row in range(len(matrix)-1):
+            for col in range(len(matrix[0])-1):
+                if matrix[row][col] != matrix[row + 1][col+1]:
                     return False
 
-       return True
+
+        return True
