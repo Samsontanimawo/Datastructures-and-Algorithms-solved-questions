598c115f3bd556fa4dc3d5228d4828ff7d1b83e2
Samson Tanimawo <samsontanimawo4jbs@yahoo.com>
2024-01-01 15:50:03 -0800

    Time: 15 ms (75.50%) | Memory: 13.4 MB (59.83%) - LeetSync

--- 65-valid-number/valid-number.py
+++ 65-valid-number/valid-number.py
@@ -1,34 +1,34 @@
 class Solution(object):
     def isNumber(self, s):
-        digit = decimal = symbol = exponent = False
+       decimal = digit = symbol = exponent = False
 
-        for char in s:
-            if char.isdigit():
-                digit = True
+       for char in s:
+           if char.isdigit():
+               digit = True
 
-            elif char in "+-":
-                if symbol or decimal or digit:
+           elif char in "+-":
+               if decimal or digit or symbol:
                     return False
-                
-                else:
+
+               else:
                     symbol = True
 
-            elif char in "eE":
+           elif char in "eE":
                 if not digit or exponent:
                     return False
 
                 else:
-                    digit = symbol = decimal = False
+                    digit = decimal = symbol = False
                     exponent = True
 
-            elif char == ".":
-                if decimal or exponent:
-                    return False
+           elif char == ".":
+               if decimal or exponent:
+                   return False
 
-                else:
-                    decimal = True
+               else:
+                    decimal = True   
 
-            else:
-                return False
+           else:
+                return False       
 
-        return digit
+       return digit
