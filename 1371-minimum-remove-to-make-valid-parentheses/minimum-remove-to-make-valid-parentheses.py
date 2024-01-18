8aafe2efd5a226461c90641d0cb2c4e8722e8611
Samson Tanimawo <samsontanimawo4jbs@yahoo.com>
2024-01-17 20:43:17 -0600

    Time: 68 ms (84.88%) | Memory: 19 MB (18.65%) - LeetSync

--- 1371-minimum-remove-to-make-valid-parentheses/minimum-remove-to-make-valid-parentheses.py
+++ 1371-minimum-remove-to-make-valid-parentheses/minimum-remove-to-make-valid-parentheses.py
@@ -1,38 +1,20 @@
 class Solution(object):
     def minRemoveToMakeValid(self, s):
-        open_count = 0
-        to_remove_left = set()
-        to_remove_right = set()
+       stack = []
+       s = list(s)
 
-    # First pass: Mark invalid left parentheses
-        for i, char in enumerate(s):
-            if char == '(':
-                open_count += 1
-            elif char == ')':
-                if open_count > 0:
-                    open_count -= 1
+       for index in range(len(s)):
+           if s[index] == "(":
+               stack.append(index)
+
+           elif s[index] == ")":
+                if stack:
+                    stack.pop()
+
                 else:
-                    to_remove_right.add(i)
+                    s[index] = ""
 
-    # Second pass: Mark invalid right parentheses
-        open_count = 0
-        for i in range(len(s) - 1, -1, -1):
-           char = s[i]
-           if char == ')':
-               open_count += 1
-               
-           elif char == '(':
-               if open_count > 0:
-                   open_count -= 1
-               else:
-                   to_remove_left.add(i)
+       for index in stack:
+            s[index] = ""
 
-    # Combine all indices to remove
-        to_remove = to_remove_left.union(to_remove_right)
-
-        result = []
-        for i, char in enumerate(s):
-           if i not in to_remove:
-               result.append(char)
-
-        return ''.join(result)
+       return "".join(s)
