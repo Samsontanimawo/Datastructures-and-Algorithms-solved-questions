d6d56822d76910c7aba9021c67463d5e5a889399
Samson Tanimawo <samsontanimawo4jbs@yahoo.com>
2024-01-18 18:28:31 -0600

    Time: 23 ms (24.31%) | Memory: 13.2 MB (98.97%) - LeetSync

--- 314-binary-tree-vertical-order-traversal/binary-tree-vertical-order-traversal.py
+++ 314-binary-tree-vertical-order-traversal/binary-tree-vertical-order-traversal.py
@@ -3,25 +3,29 @@ class Solution(object):
         if not root:
             return []
 
-        queue = deque([(root, 0)])
-        minColumn = 0
-        maxColumn = 0
+    # Initialize a hashmap to store nodes at each columnIndex
         hashmap = defaultdict(list)
+    # Initialize a deque for level-order traversal with initial root and columnIndex 0
+        queue = deque([(root, 0)])
 
+    # Initialize min and max columns to keep track of the range of columns
+        minColumn, maxColumn = 0, 0
+
+    # Perform level-order traversal
         while queue:
             node, columnIndex = queue.popleft()
+        # Append the node value to the corresponding columnIndex in the hashmap
+            hashmap[columnIndex].append(node.val)
 
-            if node:
-                hashmap[columnIndex].append(node.val)
-                minColumn = min(minColumn, columnIndex)
-                maxColumn = max(maxColumn, columnIndex)
+        # Update min and max columns
+            minColumn = min(minColumn, columnIndex)
+            maxColumn = max(maxColumn, columnIndex)
 
+        # Enqueue left and right children with updated column indices
             if node.left:
                 queue.append((node.left, columnIndex - 1))
-
             if node.right:
                 queue.append((node.right, columnIndex + 1))
 
+    # Generate the final output by iterating through the columns range
         return [hashmap[index] for index in range(minColumn, maxColumn + 1)]
-
-# O(N) Time and Space
