4e909623ac70b4403db742656b2b5e322951f92e
Samson Tanimawo <samsontanimawo4jbs@yahoo.com>
2024-03-01 19:25:18 -0600

    Time: 14 ms (75.28%) | Memory: 11.6 MB (80.81%) - LeetSync

--- 314-binary-tree-vertical-order-traversal/binary-tree-vertical-order-traversal.py
+++ 314-binary-tree-vertical-order-traversal/binary-tree-vertical-order-traversal.py
@@ -1,25 +1,25 @@
+
 class Solution(object):
     def verticalOrder(self, root):
-        queue = deque([(root, 0)])
-        minColumn = 0
-        maxColumn = 0
-        hashmap = defaultdict(list)
+       minColumn  = 0
+       maxColumn = 0
+       queue = deque([(root, 0)])
+       hashmap = defaultdict(list)
 
-        if not root:
-            return []
+       if not root:
+           return []
 
-        while queue:
-            node, columnIndex = queue.popleft()
-
-            if node:
+       while queue:
+           node, columnIndex = queue.popleft()
+           
+           if node:
                 hashmap[columnIndex].append(node.val)
                 minColumn = min(minColumn, columnIndex)
                 maxColumn = max(maxColumn, columnIndex)
 
-            if node.left:
+           if node.left:
                 queue.append((node.left, columnIndex - 1))
 
-            if node.right:
+           if node.right:
                 queue.append((node.right, columnIndex + 1))
-
-        return [hashmap[index] for index in range(minColumn, maxColumn + 1)]
+       return [hashmap[index] for index in range(minColumn, maxColumn+ 1)]
