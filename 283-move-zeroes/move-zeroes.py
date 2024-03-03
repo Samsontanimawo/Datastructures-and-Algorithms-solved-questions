7576f673cb132ff570d4a2ecb50e3c5ed22a40c6
Samson Tanimawo <samsontanimawo4jbs@yahoo.com>
2024-03-02 21:47:08 -0600

    Time: 120 ms (51.66%) | Memory: 12.8 MB (77.70%) - LeetSync

--- 283-move-zeroes/move-zeroes.py
+++ 283-move-zeroes/move-zeroes.py
@@ -1,13 +1,14 @@
 class Solution(object):
     def moveZeroes(self, nums):
         left = 0
+        right = 0
 
-        for right in range(len(nums)):
+        # Iterate through the array using a while loop
+        while right < len(nums):
+            # If a non-zero element is found, swap it with the left pointer
             if nums[right] != 0:
                 nums[left], nums[right] = nums[right], nums[left]
-                left +=1
-
-            else:
-                right -=1
+                left += 1  # Move the left pointer to the right
+            right += 1  # Move the right pointer to the right
 
         return len(nums)
