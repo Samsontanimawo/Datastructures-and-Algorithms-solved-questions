057a201b1e2b219ffe8ef6a4602d58484dad1364
Samson Tanimawo <samsontanimawo4jbs@yahoo.com>
2024-03-06 10:20:16 -0600

    Time: 10 ms (92.33%) | Memory: 11.9 MB (43.48%) - LeetSync

--- 339-nested-list-weight-sum/nested-list-weight-sum.py
+++ 339-nested-list-weight-sum/nested-list-weight-sum.py
@@ -2,13 +2,13 @@ class Solution(object):
     def depthSum(self, nestedList):
         def helper(nestedList, depth):
             sum = 0
-
+            
             for item in nestedList:
                 if item.isInteger():
                     sum += item.getInteger() * depth
 
                 else:
-                    sum += helper(item.getList(), depth + 1)
+                    sum += helper(item.getList(), depth+1)
 
             return sum
 
