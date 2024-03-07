830c95520c0af94d2120866d4c052df68ca2e51d
Samson Tanimawo <samsontanimawo4jbs@yahoo.com>
2024-03-07 06:59:17 -0600

    Time: 18 ms (31.60%) | Memory: 11.9 MB (42.39%) - LeetSync

--- 339-nested-list-weight-sum/nested-list-weight-sum.py
+++ 339-nested-list-weight-sum/nested-list-weight-sum.py
@@ -1,15 +1,24 @@
 class Solution(object):
     def depthSum(self, nestedList):
+        # Define a helper function for depth-sum calculation
         def helper(nestedList, depth):
+            # Initialize the sum to 0
             sum = 0
-            
+
+            # Iterate through each element in the nestedList
             for item in nestedList:
+                # If the current item is an integer
                 if item.isInteger():
-                    sum += item.getInteger() * depth
+                    # Add the integer value multiplied by the current depth to the sum
+                    sum += item.getInteger() * depth 
 
+                # If the current item is a nested list
                 else:
-                    sum += helper(item.getList(), depth+1)
+                    # Recursively call the helper function with the nested list and incremented depth
+                    sum += helper(item.getList(), depth + 1)
 
+            # Return the sum calculated for the current nestedList
             return sum
 
+        # Start the depth-sum calculation with the input nestedList and initial depth of 1
         return helper(nestedList, 1)
