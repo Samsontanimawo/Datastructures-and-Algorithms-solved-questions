f09185dc127d987934ea54a1ea5e16442d4061eb
Samsontanimawo <engr.tanimawo@yahoo.com>
2024-03-11 21:17:36 -0500

    Added main system to run the code

--- 162-find-peak-element/find-peak-element.py
+++ 162-find-peak-element/find-peak-element.py
@@ -1,20 +1,19 @@
-# Peak number is a number that's bigger than its neighbor
-
 class Solution(object):
     def findPeakElement(self, nums):
         left = 0
-        right = len(nums)-1
+        right = len(nums) - 1
 
         while left < right:
-            mid = (left + right)//2 
+            mid = (left + right) // 2 
             
             if nums[mid] < nums[mid + 1]:
                 left = mid + 1
-
             else:
                 right = mid
 
-        return left 
+        return left
 
-test = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0]
+if __name__ == "__main__":
+
+test = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 1]
 print(Solution().findPeakElement(test))
