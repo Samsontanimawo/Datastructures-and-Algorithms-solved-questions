df8f9147ae0d93cf358120712aa2a810613b697b
Samsontanimawo <engr.tanimawo@yahoo.com>
2024-03-11 22:27:50 -0500

    Updated move zeros code

--- 283-move-zeroes/move-zeroes.py
+++ 283-move-zeroes/move-zeroes.py
@@ -1,12 +1,28 @@
+
+# MOVE ZEROS TO LEFT
+# Input = [ 1, 0, 2, 0, 0, 0, 1]
+# Ouput = [0, 0, 0, 0, 1, 1, 2]
+
+
+# MOVE ZEROS TO RIGHT
+# Ouput = [0, 0, 0, 0, 1, 1, 2]
+# Input = [ 1, 1, 2, 0, 0, 0, 0]
+
 class Solution(object):
-    def moveZeroes(self, nums):
-        left = right = 0
+  def move_zeros_to_left_and_right(self, nums):
+    left = len(nums) - 1
+    right = len(nums)-1
 
-        while right < len(nums):
-            if nums[right] != 0:
-                nums[left], nums[right] = nums[right], nums[left]
-                left +=1
+    while left >= 0:
+ #   while left >= 0: # Replace right with left if you want the zeros to move to right 
+      if nums[right] != 0:
+        nums[right], nums[left] = nums[left], nums[right]
+        left -=1
 
-            right +=1
+      right -=1
 
-        return len(nums)
+    return nums
+
+test = [8, 0, 0, 0, 1, -3, 12]
+
+print(Solution().move_zeros_to_left_and_right(test))
