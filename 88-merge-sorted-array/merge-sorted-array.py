8bbf8eab0dc9f5fca2906e17da54e791f786fca7
Samson Tanimawo <engr.tanimawo@yahoo.com>
2024-03-14 20:05:11 -0500

    Time: 20 ms (50.05%) | Memory: 11.5 MB (83.10%) - LeetSync

--- 88-merge-sorted-array/merge-sorted-array.py
+++ 88-merge-sorted-array/merge-sorted-array.py
@@ -1,14 +1,17 @@
 class Solution(object):
     def merge(self, nums1, m, nums2, n):
         while m>0 and n>0:
-            if nums1[m-1] > nums2[n-1]:
-                nums1[m+n-1] = nums1[m-1]
-                m -=1
-
-            else:
+            if nums1[m-1] < nums2[n-1]:
                 nums1[m+n-1] = nums2[n-1]
                 n -=1
+
+            else:
+                nums1[m+n-1] = nums1[m-1]
+                m -=1
 
         while n> 0:
             nums1[m+n-1] = nums2[n-1]
             n -=1
+
+# Time complexity: O(n+m)
+# Space complexity: O(1)
