a4e1bec2bf96f2430352e907cabd63a30c2705cc
Samson Tanimawo <engr.tanimawo@yahoo.com>
2024-03-21 18:29:28 -0500

    Time: 129 ms (46.57%) | Memory: 18.8 MB (30.23%) - LeetSync

--- 540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.py
+++ 540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.py
@@ -1,11 +1,26 @@
-class Solution(object):
+class Solution:
     def singleNonDuplicate(self, nums):
-        result = 0
-
-        for num in nums:
-            result = num ^ result
-
-        return result
-
-        # O(N) TIME | o(1) space
+        left, right = 0, len(nums) - 1
         
+        while left < right:
+            mid = left + (right - left) // 2
+            if mid % 2 == 0:
+                if nums[mid] == nums[mid + 1]:
+                    left = mid + 2
+                else:
+                    right = mid
+            else:
+                if nums[mid] == nums[mid - 1]:
+                    left = mid + 1
+                else:
+                    right = mid - 1
+        
+        return nums[left]  # or nums[right]
+
+# Test cases
+nums1 = [1,1,2,3,3,4,4,8,8]
+nums2 = [3,3,7,7,10,11,11]
+
+solution = Solution()
+print(solution.singleNonDuplicate(nums1))  # Output: 2
+print(solution.singleNonDuplicate(nums2))  # Output: 10
