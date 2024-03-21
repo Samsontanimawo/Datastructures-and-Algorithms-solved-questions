6dd3fc4bc6c2712dcbc8f89bcf602b06ac0c4fae
Samson Tanimawo <engr.tanimawo@yahoo.com>
2024-03-21 11:48:05 -0500

    Time: 16 ms (43.22%) | Memory: 11.8 MB (36.60%) - LeetSync

--- 408-valid-word-abbreviation/valid-word-abbreviation.py
+++ 408-valid-word-abbreviation/valid-word-abbreviation.py
@@ -1,25 +1,28 @@
-
+# Input: word = "internationalization", abbr = "i12iz4n"
+#                L                              R
 
 class Solution(object):
     def validWordAbbreviation(self, word, abbr):
-        left, right = 0, 0
+        left = 0
+        right = 0
+
         while left < len(word) and right < len(abbr):
-            if word[left] == abbr[right]:
-                left += 1
-                right += 1
-            elif abbr[right].isdigit():
-                if abbr[right] == '0':  # Check for leading zeros
+            if word[left] != abbr[right]:
+                if not abbr[right].isdigit() or abbr[right] == "0":
                     return False
-                steps = 0
+
+                else:
+                    steps = 0
+
                 while right < len(abbr) and abbr[right].isdigit():
                     steps = steps * 10 + int(abbr[right])
-                    right += 1
-                left += steps
-            else:
-                return False
-        return left == len(word) and right == len(abbr)
+                    right +=1
 
-# Test cases
-solution = Solution()
-print(solution.validWordAbbreviation("internationalization", "i12iz4n"))  # Output: True
-print(solution.validWordAbbreviation("apple", "a2e"))  # Output: False
+                left += steps
+
+            else:
+                left +=1
+                right +=1
+
+        return left == len(word) and right == len(abbr)
+        
