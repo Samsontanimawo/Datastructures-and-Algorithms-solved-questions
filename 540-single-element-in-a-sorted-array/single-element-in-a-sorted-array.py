33779e1d28b8bf04693053bc67d56b0b886da7a9
Samson Tanimawo <engr.tanimawo@yahoo.com>
2024-03-21 18:30:12 -0500

    Time: 122 ms (78.10%) | Memory: 18.9 MB (11.76%) - LeetSync

--- 540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.py
+++ 540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.py
@@ -3,7 +3,7 @@ class Solution:
         left, right = 0, len(nums) - 1
         
         while left < right:
-            mid = left + (right - left) // 2
+            mid = (right + left) // 2
             if mid % 2 == 0:
                 if nums[mid] == nums[mid + 1]:
                     left = mid + 2
