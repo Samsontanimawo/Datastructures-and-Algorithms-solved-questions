f7dec6ff9aa1216c14fe850e924cad85b7dcb77d
Samson Tanimawo <engr.tanimawo@yahoo.com>
2024-03-23 07:18:54 -0500

    Time: 113 ms (14.79%) | Memory: 16.8 MB (54.32%) - LeetSync

--- 56-merge-intervals/merge-intervals.py
+++ 56-merge-intervals/merge-intervals.py
@@ -1,24 +1,22 @@
 class Solution(object):
-    # Function to merge overlapping intervals
     def merge(self, intervals):
-        # Check if the intervals list is empty
         if not intervals:
-            return None  # If empty, return None
-        
-        start = 0  # Index of start time in interval
-        end = 1    # Index of end time in interval
-        result = []  # List to store the merged intervals
+            return None
 
-        intervals.sort()  # Sort the intervals based on start times
+        start = 1
+        end = -1
 
-        # Loop through each interval in sorted intervals
+        intervals.sort()
+
+        result = []
+
         for interval in intervals:
-            # Check if the result list is empty or the current interval starts after the end of the last interval in the result list
-            if not result or result[-1][end] < interval[start]:
-                result.append(interval)  # If so, add the current interval to the result list
-                
-            else:
-                # If the current interval overlaps with the last interval in the result list, merge them
-                result[-1][end] = max(result[-1][end], interval[end])  # Update the end time of the last interval in result list
+            if not result or result[end][start] < interval[0]:
+                result.append(interval)
 
-        return result  # Return the merged intervals
+            else:
+                result[end][start] = max(result[end][start], interval[end])
+
+        return result
+
+# O(N) TIME + O(1) space
