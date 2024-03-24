cb8f30f76351335f2e63fae645f65acecda93622
Samson Tanimawo <engr.tanimawo@yahoo.com>
2024-03-24 13:44:51 -0500

    Time: 65 ms (92.84%) | Memory: 17.3 MB (20.61%) - LeetSync

--- 1371-minimum-remove-to-make-valid-parentheses/minimum-remove-to-make-valid-parentheses.py
+++ 1371-minimum-remove-to-make-valid-parentheses/minimum-remove-to-make-valid-parentheses.py
@@ -1,26 +1,38 @@
 class Solution(object):
     def minRemoveToMakeValid(self, s):
+        # Initialize a variable to keep track of the balance of parentheses
         balance = 0
+        
+        # Convert the input string into a list of characters
         s = list(s)
         
         # First pass: Remove excess closing parentheses
-        for i in range(len(s)):
-            if s[i] == '(':
+        for index in range(len(s)):
+            # If the current character is an opening parenthesis, increase the balance
+            if s[index] == '(':
                 balance += 1
-            elif s[i] == ')':
+            # If the current character is a closing parenthesis
+            elif s[index] == ')':
+                # If there are no corresponding opening parentheses for this closing one, remove it
                 if balance == 0:
-                    s[i] = ''
+                    s[index] = ''
+                # Otherwise, decrease the balance
                 else:
                     balance -= 1
         
         # Second pass: Remove excess opening parentheses
-        for i in range(len(s) - 1, -1, -1):
+        # Traverse the string backward
+        for index in range(len(s) - 1, -1, -1):
+            # If the balance is already restored to zero, exit the loop
             if balance == 0:
                 break
-            if s[i] == '(':
-                s[i] = ''
+            # If the current character is an opening parenthesis
+            if s[index] == '(':
+                # Remove it and decrement the balance
+                s[index] = ''
                 balance -= 1
         
+        # Convert the modified list back into a string and return it
         return ''.join(s)
 
 # Example usage:
