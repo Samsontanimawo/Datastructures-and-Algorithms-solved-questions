8abc1e2f1a04b9997bab6995910ddb8ad223efdb
Samson Tanimawo <engr.tanimawo@yahoo.com>
2024-03-26 13:29:31 -0500

    Time: 15 ms (48.49%) | Memory: 11.5 MB (79.15%) - LeetSync

--- 50-powx-n/powx-n.py
+++ 50-powx-n/powx-n.py
@@ -1,21 +1,23 @@
-class Solution:
+class Solution(object):
     def myPow(self, x, n):
         if n == 0:
             return 1
+
+        if n == 1:
+            return x
+
+        if n == -1:
+            return 1/x
+        
         if n < 0:
             x = 1 / x
             n = -n
-        result = 1
 
+        result = 1
         while n > 0:
             if n % 2 == 1:
                 result *= x
             x *= x
             n //= 2
+        
         return result
-
-# Test cases
-solution = Solution()
-print(solution.myPow(2.00000, 10))  # Output: 1024.00000
-print(solution.myPow(2.10000, 3))   # Output: 9.26100
-print(solution.myPow(2.00000, -2))  # Output: 0.25000
