c5a99a0d487420123bdbf29023af0118b831f532
Samson Tanimawo <engr.tanimawo@yahoo.com>
2024-03-26 02:53:32 -0500

    Time: 76 ms (52.15%) | Memory: 15 MB (78.10%) - LeetSync

--- 347-top-k-frequent-elements/top-k-frequent-elements.py
+++ 347-top-k-frequent-elements/top-k-frequent-elements.py
@@ -1,46 +1,44 @@
 from collections import Counter
 
 class Solution:
-
+    def topKFrequent(self, nums, k):
         # Step 1: Count the frequency of each element in the input list
         freq = Counter(nums)
         
         # Step 2: Create a list of unique elements
-        unique = list(freq.keys())
+        nums = list(freq.keys())
         
         # Calculate the position where the kth frequent element would be in the sorted list
-        k = len(unique) - k
+        k = len(nums) - k
         
         # Step 3: Define a helper function for quick selection
         def quickSelect(left, right):
             # Choose the pivot element (rightmost element in this case)
-            pivot = unique[right]
+            pivot = nums[right]
             partition = left
             
-            # Partition the unique list based on the frequency of elements
+            # Partition the nums list based on the frequency of elements
             for index in range(left, right):
-                if freq[unique[index]] <= freq[pivot]:
-                    unique[index], unique[partition] = unique[partition], unique[index]
+                if freq[nums[index]] <= freq[pivot]:
+                    nums[index], nums[partition] = nums[partition], nums[index]
                     partition += 1
                     
             # Move the pivot element to its final sorted position
-            unique[partition], unique[right] = unique[right], unique[partition]
+            nums[partition], nums[right] = nums[right], nums[partition]
             
             # Recursively perform quick select on the appropriate partition
             if partition > k:
                 return quickSelect(left, partition - 1)
-            
             elif partition < k:
                 return quickSelect(partition + 1, right)
-            
             else:
-                return unique[partition]
+                return nums[partition]
             
         # Step 4: Perform quick selection to find the kth frequent element
-        quickSelect(0, len(unique) - 1)
+        quickSelect(0, len(nums) - 1)
         
         # Step 5: Return the top k frequent elements
-        return unique[k:]
+        return nums[k:]
 
 # Additional information in comments
 """
