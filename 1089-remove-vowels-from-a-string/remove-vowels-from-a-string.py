4e630075acad2dbed190ef0dd68e5bea17f419a0
Samson Tanimawo <engr.tanimawo@yahoo.com>
2024-03-25 21:33:39 -0500

    Time: 20 ms (15.11%) | Memory: 11.7 MB (31.65%) - LeetSync

--- 1089-remove-vowels-from-a-string/remove-vowels-from-a-string.py
+++ 1089-remove-vowels-from-a-string/remove-vowels-from-a-string.py
@@ -1,11 +1,11 @@
+# Find the words that are not vowels
+
 class Solution(object):
-    def removeVowels(self, s):
-        vowels = "aAeEiIoOuU"
-        result = []
+  def removeVowels(self, s):
+    s = list(s)
+    left = 0
+    right = len(s)-1
+    vowels = "aAeEiIoOuU"
 
-        for char in s:
-            if char not in vowels:
-                result.append(char)
-
-        return "".join(result)
+    return "".join(char for char in s if char not in vowels)
 
