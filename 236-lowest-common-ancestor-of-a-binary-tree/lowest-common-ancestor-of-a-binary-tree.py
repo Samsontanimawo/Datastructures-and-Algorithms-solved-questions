841d381783f391a1c426abd271ce279991a4f807
Samson Tanimawo <engr.tanimawo@yahoo.com>
2024-12-10 16:59:58 -0600

    Time: 40 ms (84.68%) | Memory: 24.8 MB (41.99%) - LeetSync

--- 236-lowest-common-ancestor-of-a-binary-tree/lowest-common-ancestor-of-a-binary-tree.py
+++ 236-lowest-common-ancestor-of-a-binary-tree/lowest-common-ancestor-of-a-binary-tree.py
@@ -1,17 +1,24 @@
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
 class Solution(object):
-  def lowestCommonAncestor(self, root, p, q):
-    if not root:
-      return None
+    def lowestCommonAncestor(self, root, p, q):
+        if not root:
+            return None
 
-    if root == p or root == q:
-        return root
+        if root == p or root == q:
+            return root
 
-    else:
-      left = self.lowestCommonAncestor(root.left, p, q)
-      right = self.lowestCommonAncestor(root.right, p, q)
+        else:
+            left = self.lowestCommonAncestor(root.left, p, q)
+            right = self.lowestCommonAncestor(root.right, p, q)
 
-    if left and right:
-      return root
-    
-    else:
-      return left or right
+        if left and right:
+            return root
+
+        else:
+            return left or right
