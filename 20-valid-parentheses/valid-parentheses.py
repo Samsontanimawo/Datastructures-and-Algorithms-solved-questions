09500730d7aaf47c58dae3d66276e6923242e44d
Samson Tanimawo <engr.tanimawo@yahoo.com>
2025-03-08 17:06:27 -0600

    Time: 0 ms (100.00%) | Memory: 12.5 MB (84.93%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.py
+++ 20-valid-parentheses/valid-parentheses.py
@@ -1,18 +1,19 @@
-class Solution(object):
+class Solution:
     def isValid(self, s):
-        brackets = { ")":"(", "]":"[","}":"{" }
-        stack = []
-        
+
+        brackets = { "]":"[", "}":"{", ")":"("}
+
+        stack = []      # [ [             ] string = "[({)][{}{"
+
         for char in s:
             if char in brackets:
                 if stack and stack[-1] == brackets[char]:
                     stack.pop()
+
                 else:
-                    return False
+                    return False 
 
             else:
                 stack.append(char)
 
         return stack == []
-
-            
