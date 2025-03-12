2fd2e47f3abb749d68bb3fabc8ca63eaf8cfe54a
Samson Tanimawo <engr.tanimawo@yahoo.com>
2025-03-12 16:46:58 -0500

    Time: 11 ms (76.50%) | Memory: 23.8 MB (86.34%) - LeetSync

--- 217-contains-duplicate/contains-duplicate.py
+++ 217-contains-duplicate/contains-duplicate.py
@@ -1,19 +1,12 @@
 class Solution(object):
     def containsDuplicate(self, nums):
-        nums.sort()
+        duplicate = {}
 
-        left = 0
-        right = len(nums)-1
-
-        while left < len(nums)-1:
-            if nums[left] == nums[left + 1]:
+        for num in nums:
+            if num in duplicate:
                 return True
 
             else:
-                left +=1
-                right -=1
+                duplicate[num] = 1
 
         return False
-
-# O(nlogN) time = Sorting 
-# O(1) SPACE
