bc65a28e6a293825d97a125c537b226e6e56d5d3
Samson Tanimawo <engr.tanimawo@yahoo.com>
2025-03-14 11:19:07 -0500

    Time: 0 ms (100.00%) | Memory: 12.6 MB (30.46%) - LeetSync

--- 199-binary-tree-right-side-view/binary-tree-right-side-view.py
+++ 199-binary-tree-right-side-view/binary-tree-right-side-view.py
@@ -1,17 +1,28 @@
-# Travel the tree level by leve.
-# How do we keep track of the node?
-# Should I use stack or queue. DFS OR BFS. Since it's level by level. Then it's BFS:
-# Use a qeue = FIFO = pop nodes
-# 
-#         self.right = right
+# DATA STRUCTURES
+# ALGORITHMS
+# TIME AND SPACE COMPLEXITY
+# Standing on the right side of a tree
+# Return the values I can see
+# Ignore the values I can't see.
+# If i'm on the right side, I can't see any left side value
+# What algorithm should I use?
+# Keep track of the root. If there's no root, what should I return?
+# We can traverse a tree in DFS or BFS. Which one should I use?
+# Since we don't care about the left side values, let's use BFS
+# If we're using BFS, then let's use QUEUE.
+# Put the first value in the queue
+# Keep track of the levels and reset it
+# Put pop the nodes in the queue and add them to result list
+# Return the list
+
 class Solution(object):
     def rightSideView(self, root):
         if not root:
             return []
 
+        queue = [root]
         level = []
         result = []
-        queue = [root]
 
         while queue:
             for node in queue:
@@ -27,6 +38,7 @@ class Solution(object):
 
         return result
 
-# O(N) time and space
+        # O(N) TIME AND SPACE
+
 
         
