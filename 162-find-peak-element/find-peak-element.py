92b25090760681823925d5c6d94104b4a72cc29b
Samson Tanimawo <engr.tanimawo@yahoo.com>
2025-03-15 13:11:28 -0500

    Time: 4 ms (4.04%) | Memory: 12.6 MB (51.73%) - LeetSync

--- 162-find-peak-element/find-peak-element.py
+++ 162-find-peak-element/find-peak-element.py
@@ -1,18 +1,17 @@
+# What is a peak element?
+# Return the index of any of the peaks
+# Input: nums = [1,2,3,1] | nums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 1]
+# # Output = 3 | 9
+# How can we solve this problem?
+# This is array related problem
+# Sort the numbers and return the largest. Nums.sort(), return nums[-1]
+#
+#
+#
+#
+#
 class Solution(object):
     def findPeakElement(self, nums):
-        left, right = 0, len(nums)-1
-
-        while left < right:
-            mid = (left + right)//2 # left - (left + right)//2
-
-            if nums[mid] < nums[mid+1]:
-                left = mid + 1
-
-            else:
-                right = mid 
-
-        return left
-
-# TIME COMPLEXITY = O(logN)
-# Space complexity = O(1)
-        
+        sorted_nums = sorted(nums)  # Sort a copy to avoid modifying the original
+        peak_value = sorted_nums[-1]  # Get the largest element (a peak)
+        return nums.index(peak_value) 
