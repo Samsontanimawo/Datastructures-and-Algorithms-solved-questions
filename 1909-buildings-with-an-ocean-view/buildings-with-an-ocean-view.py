38f6ee6aba48f459b58ef2065a5442cc3f6233b5
Samson Tanimawo <engr.tanimawo@yahoo.com>
2025-03-16 18:58:38 -0500

    Time: 15 ms (99.32%) | Memory: 28.4 MB (41.72%) - LeetSync

--- 1909-buildings-with-an-ocean-view/buildings-with-an-ocean-view.py
+++ 1909-buildings-with-an-ocean-view/buildings-with-an-ocean-view.py
@@ -3,10 +3,10 @@ class Solution(object):
         if not heights:
             return []
 
-        result = []
         maxHeight = 0
+        result = []
 
-        for index in range(len(heights)-1,-1,-1):
+        for index in range(len(heights)- 1,-1,-1):
             if heights[index] > maxHeight:
                 result.append(index)
                 maxHeight = heights[index]
