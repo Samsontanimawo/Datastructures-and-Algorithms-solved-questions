869852494907a2d2c1a3be3167a7176a6830f801
Samson Tanimawo <engr.tanimawo@yahoo.com>
2025-03-16 17:44:13 -0500

    Time: 12 ms (62.13%) | Memory: 26 MB (15.32%) - LeetSync

--- 217-contains-duplicate/contains-duplicate.py
+++ 217-contains-duplicate/contains-duplicate.py
@@ -1,3 +1,23 @@
+"""
+Input: nums = [1,2,3,1]
+Use set = [1, 2, 3]
+
+return len(nums) != len(set(nums))
+
+Time and Space Complexity Analysis
+Time Complexity:
+Creating a set from nums → set(nums) takes O(N) time, where N is the length of nums.
+Computing len(nums) and len(set(nums)) → Both are O(1) operations.
+Overall Complexity: O(N)
+Space Complexity:
+set(nums) stores unique elements → In the worst case, if all elements are distinct, the set will contain N elements.
+Extra space used is O(N) for the set.
+Overall Complexity: O(N)
+
+"""
+
+
 class Solution(object):
     def containsDuplicate(self, nums):
         return len(nums) != len(set(nums))
+        
