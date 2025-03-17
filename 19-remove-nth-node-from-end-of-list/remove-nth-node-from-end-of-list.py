84a1423047b931b38712de8d110cde38c0d0b22f
Samson Tanimawo <engr.tanimawo@yahoo.com>
2025-03-16 19:17:38 -0500

    Time: 0 ms (100.00%) | Memory: 12.6 MB (20.84%) - LeetSync

--- 19-remove-nth-node-from-end-of-list/remove-nth-node-from-end-of-list.py
+++ 19-remove-nth-node-from-end-of-list/remove-nth-node-from-end-of-list.py
@@ -1,18 +1,30 @@
-class Solution:
+class Solution(object):
     def removeNthFromEnd(self, head, n):
-        
-        slow = fast = head
-        
+        # Initialize two pointers: 'fast' and 'slow' both at the head of the list
+        fast = slow = head
+
+        # Move the 'fast' pointer 'n' steps ahead
         for index in range(n):
-            fast = fast.next 
-            
+            fast = fast.next  # Move the fast pointer one step ahead
+            # If n = 2, move the second pointer to position 2
+
+        # If fast is None, it means the 'n'th node from the end is the head node
+        # We just need to remove the head and return the new head
         if not fast:
-            return slow.next 
+            return slow.next  # Remove the head node and return the next node
 
+        # Move both fast and slow pointers one step at a time
+        # Now fast is 'n' nodes ahead, so when fast reaches the end,
+        # slow will be at the node just before the one to be removed
         while fast.next:
-            fast = fast.next 
-            slow = slow.next 
+            fast = fast.next  # Move the fast pointer one step forward
+            slow = slow.next  # Move the slow pointer one step forward
 
-        slow.next = slow.next.next 
-            
-        return head 
+        # Now, slow is at the node just before the one to be removed
+        slow.next = slow.next.next  # Remove the 'n'th node from the end by skipping it
+
+        # Return the head of the modified list
+        return head
+
+# Time Complexity: O(N), where N is the length of the linked list
+# Space Complexity: O(1), because we are only using two pointers and not any additional space
