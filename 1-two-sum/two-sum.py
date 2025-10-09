1a56e4880793c7ae91b3ba956789e24b6e2255f7
Samson Tanimawo <engr.tanimawo@yahoo.com>
2025-10-09 18:50:05 -0500

    Time: 0 ms (100.00%) | Memory: 12.9 MB (97.99%) - LeetSync

--- 1-two-sum/two-sum.py
+++ 1-two-sum/two-sum.py
@@ -1,19 +1,11 @@
-class Solution:
+class Solution(object):
     def twoSum(self, nums, target):
-        
-        hashmap = {}
-        
-        for index, num in enumerate(nums):
-            diff = target - nums[index]
-            
-            if diff in hashmap:
-                return [hashmap[diff], index]
-            
-            else:
-                hashmap[num] = index
-                
-                """
-                O(N) Time and Space
-                
-                """
+        lookup = {}
+
+        for i, num in enumerate(nums):
+            diff = target - num
+            if diff in lookup:
+                return [lookup[diff], i]
+
+            lookup[num] = i
         
